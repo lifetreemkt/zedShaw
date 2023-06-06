ca87c190bdbe8ef01929a40731da7e1e0691964a
lifetreemkt <carlos.rodrigues@lifetreemarketing.com>
2023-06-06 20:53:38 -0300

    save

--- homeWork3.py
+++
@@ -1,9 +0,0 @@
-def topseller (tvs, consoles, tablets):
-    if tvs > consoles and tvs > tablets:
-        print ('tv is the topseller')
-    elif consoles > tvs and consoles > tablets:
-        print ('consoles is the topseller')
-    else:
-        print ('tablet is the topseller')
-        
-topseller (120, 220, 115)
--- homeWork4.py
+++
@@ -1,9 +0,0 @@
-myUniqueList = ["orange", "apple"]
-fruitName = input("Please enter a fruit name:\n")
-if fruitName in myUniqueList:
-    print ("fruits already on list")
-else:
-    myUniqueList.append(fruitName)
-    print ("true - fruit added to list")
-
-print (myUniqueList)
